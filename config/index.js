324e5bbebc0afcf576a84580827c235b80ff9737
PNFeather <921192114@qq.com>
2019-06-04 11:09:37 +0800

    提交

--- config/index.js
+++ config/index.js
@@ -10,10 +10,18 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
-
+    proxyTable: {
+      '/apis': {
+        // 测试环境
+        target: 'http://www.test.cn/',  // 接口域名
+        changeOrigin: true,  //  是否跨域
+        pathRewrite: {
+          '^/apis': ''   // 需要rewrite重写的,
+        }
+      }
+    },
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
@@ -50,7 +58,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import http from '@/plugins/http';
+import store from './store/';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
 
-Vue.config.productionTip = false
+process.env.MOCK && require('@/mock');
+
+Vue.use(http.install);
+
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
-})
+});
