15af2f32cbb0ef151798fa86172a0f9c5f8a6b7c
PNFeather <921192114@qq.com>
2019-06-04 14:06:58 +0800

    提交

---
+++ src/page/login/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>登录</div>
+</template>
+
+<script type='text/babel'>
+  export default {
+    name: 'login',
+    data () {
+      return {
+      };
+    },
+    created () {},
+    mounted () {},
+    computed: {},
+    watch: {},
+    methods: {},
+    components: {}
+  };
+</script>
+<style scoped lang="less">
+  [name = 'login']{}
+</style>
