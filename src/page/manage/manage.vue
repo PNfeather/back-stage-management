4f2bdaf3a57bf5f84aedf283c654bc4309092edd
PNFeather <921192114@qq.com>
2019-06-04 16:47:44 +0800

    提交

--- src/page/manage/manage.vue
+++ src/page/manage/manage.vue
@@ -1,29 +1,70 @@
 <template>
-  <div name='manage'>
+  <div name='manage' class="fillcontain">
+    <el-row style="height: 100%;">
+      <el-col :span="4"  style="min-height: 100%; background-color: #324057;">
+        <el-menu :default-active="defaultActive" style="min-height: 100%;" theme="dark" router>
+          <el-menu-item index="manage"><i class="el-icon-menu"></i>管理后台</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title"><i class="iconfont iconAccountManagement"></i>账号管理</template>
+            <!--<el-menu-item index="userList">用户列表</el-menu-item>-->
+            <!--<el-menu-item index="shopList">商家列表</el-menu-item>-->
+            <!--<el-menu-item index="foodList">食品列表</el-menu-item>-->
+            <!--<el-menu-item index="orderList">订单列表</el-menu-item>-->
+            <!--<el-menu-item index="adminList">管理员列表</el-menu-item>-->
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"><i class="iconfont iconClassManagement"></i>班级管理</template>
+            <!--<el-menu-item index="addShop">添加商铺</el-menu-item>-->
+            <!--<el-menu-item index="addGoods">添加商品</el-menu-item>-->
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title"><i class="iconfont iconTask"></i>作业管理</template>
+            <!--<el-menu-item index="visitor">用户分布</el-menu-item>-->
+            <!-- <el-menu-item index="newMember">用户数据</el-menu-item> -->
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title"><i class="iconfont iconResourceManagement"></i>资源管理</template>
+            <!-- <el-menu-item index="uploadImg">上传图片</el-menu-item> -->
+            <!--<el-menu-item index="vueEdit">文本编辑</el-menu-item>-->
+          </el-submenu>
+          <el-submenu index="6">
+            <template slot="title"><i class="iconfont iconSchoolDictionary"></i>字典管理</template>
+            <!--<el-menu-item index="adminSet">管理员设置</el-menu-item>-->
+            <!-- <el-menu-item index="sendMessage">发送通知</el-menu-item> -->
+          </el-submenu>
+          <el-submenu index="7">
+            <template slot="title"><i class="iconfont iconTickling"></i>反馈信息</template>
+            <!--<el-menu-item index="explain">说明</el-menu-item>-->
+          </el-submenu>
+        </el-menu>
+      </el-col>
+      <el-col :span="20" style="height: 100%;overflow: auto;">
+        <keep-alive>
+          <router-view></router-view>
+        </keep-alive>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script type='text/babel'>
   export default {
-    name: 'manage',
-    props: {
-      key: {
-        type: String,
-        default: ''
+    computed: {
+      defaultActive () {
+        return this.$route.path.replace('/', '');
       }
-    },
-    data () {
-      return {
-      };
-    },
-    created () {},
-    mounted () {},
-    computed: {},
-    watch: {},
-    methods: {},
-    components: {}
+    }
   };
 </script>
 <style scoped lang="less">
-  [name = 'manage']{}
+  @import '~@/style/mixin';
+  [name = 'manage']{
+    .iconfont {
+      vertical-align: middle;
+      margin-right: 5px;
+      width: 24px;
+      text-align: center;
+      display: inline-block;
+    }
+  }
 </style>
