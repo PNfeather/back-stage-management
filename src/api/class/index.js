fdbb777c6945854d2cca38730bf3491a95feecc8
PNFeather <921192114@qq.com>
2019-06-10 15:35:25 +0800

    提交

--- src/api/class/index.js
+++ src/api/class/index.js
@@ -1,5 +1,5 @@
 import {Http} from '@/plugins/http';
 
-export const getList = data => Http.post('/class/list', data);
+export const getList = data => Http.get('/class/list', {params: data});
 export const getStudents = classId => Http.get('/class/' + classId + '/students');
 export const getTeachers = classId => Http.get('/class/' + classId + '/teachers');
--- src/api/template/index.js
+++ src/api/template/index.js
@@ -1,5 +1,5 @@
 import {Http} from '@/plugins/http';
 
 export const getList = data => Http.post('/template/list', data);
-export const deleteResource = data => Http.delete('/template/delete', {data: data});
+export const deleteResource = data => Http.delete('/template/delete', {params: data});
 export const getTemplateDetail = (bookId) => Http.get('/template/' + bookId + '/template-book-detail');
