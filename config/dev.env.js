212b7648cd16618d0eee3e74c6a066ce63f600d6
PNFeather <921192114@qq.com>
2019-06-19 14:09:38 +0800

    提交

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   MOCK: 'true',
-  BASE_URL: '"/api"'
-  // BASE_URL: '"http://cjapi.ecaiedu.com/api/admin"'
+  // BASE_URL: '"/api"'
+  BASE_URL: '"http://cjapi.ecaiedu.com/api/admin"'
 })
