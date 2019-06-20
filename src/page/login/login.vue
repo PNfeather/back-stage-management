88cbbc3c8898d21350a77c97d8f4175a106fd690
PNFeather <921192114@qq.com>
2019-06-20 17:15:55 +0800

    提交

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -7,7 +7,7 @@
         </div>
         <el-form :model="loginForm" :rules="rules" ref="loginForm">
           <el-form-item prop="account">
-            <el-input v-model="loginForm.account" placeholder="账号"></el-input>
+            <el-input v-model="loginForm.account" placeholder="账号11111"></el-input>
           </el-form-item>
           <el-form-item prop="password">
             <el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
