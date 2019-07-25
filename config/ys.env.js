e46f4b4f6260b8f36b57139f327e642f7f948d68
PNFeather <921192114@qq.com>
2019-07-25 17:49:22 +0800

    提交

--- config/ys.env.js
+++ config/ys.env.js
@@ -2,7 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   MOCK: 'false',
-  BASE_URL: '"http://demo.ecaiedu.com/api"',
+  BASE_URL: '"http://demoapi.ecaiedu.com/api"',
   XZDKF_ENTRANCE: '"http://demo.ecaiedu.com/service/#/login"',
   CJIMGURL: '"https://cjfc-prod.oss-cn-shenzhen.aliyuncs.com/"'
 }
