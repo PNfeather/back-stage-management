c0fafdccfcd528b9d13a1ab17d65e5b916464fdd
PNFeather <921192114@qq.com>
2019-08-23 14:43:06 +0800

    安卓下载页面rem单位

--- src/page/download/android.vue
+++ src/page/download/android.vue
@@ -42,6 +42,13 @@
     created () {
       this.pageInit();
     },
+    mounted () {
+      this.setRem();
+    },
+    beforeRouteLeave (to, from, next) {
+      document.getElementsByTagName('html')[0].removeAttribute('style');
+      next();
+    },
     methods: {
       pageInit () {
         let ua = window.navigator.userAgent.toLowerCase();
@@ -58,6 +65,12 @@
             this.$message.error(data.message);
           }
         });
+      },
+      setRem () {
+        let whdef = 20 / 375;// 表示1450的设计图,使用10PX的默认值
+        let bodyWidth = document.body.clientWidth;// 当前窗口的宽度
+        let rem = bodyWidth * whdef;// 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
+        document.getElementsByTagName('html')[0].style.fontSize = rem + 'px';
       },
       getSize (size) {
         let KB = size / 1024;
@@ -91,31 +104,31 @@
       z-index: 99;
       .leadArrows{
         position: absolute;
-        right: 30px;
+        right: 1.5rem;
         top: 0;
-        height: 150px;
+        height: 7.5rem;
         width: auto;
       }
       .leadText{
         color: #fff;
         text-align: right;
-        font-size: 14px;
+        font-size: .7rem;
         position: absolute;
-        top: 155px;
-        right: 5px;
+        top: 7.8rem;
+        right: .25rem;
       }
     }
     .header{
-      .wh(100%, 222px);
+      .wh(100%, 11.1rem);
       position: relative;
       background-image: linear-gradient(-135deg, #07D3F5 0%, #0DB2EF 100%);
       &:before{
         content: '';
         position: absolute;
         top: 0;
-        right: 5px;
-        left: 5px;
-        bottom: -10px;
+        right: .25rem;
+        left: .25rem;
+        bottom: -.5rem;
         z-index: 2;
         opacity: 0.3;
         background-image: linear-gradient(-135deg, #07D3F5 0%, #0DB2EF 100%);
@@ -124,9 +137,9 @@
         content: '';
         position: absolute;
         top: 0;
-        right: 11px;
-        left: 11px;
-        bottom: -20px;
+        right: .55rem;
+        left: .55rem;
+        bottom: -1rem;
         z-index: 2;
         opacity: 0.3;
         background-image: linear-gradient(-135deg, #07D3F5 0%, #0DB2EF 100%);
@@ -138,25 +151,25 @@
         color: #fff;
         z-index: 3;
         .logo{
-          .wh(72px, 72px);
-          margin: 65px auto 0;
+          .wh(3.6rem, 3.6rem);
+          margin: 3.25rem auto 0;
           .logoImg{
             .wh(100%, 100%)
           }
         }
         .name{
-          .ft(18px, 25px);
-          margin-top: 13px;
+          .ft(.9rem, 1.25rem);
+          margin-top: .65rem;
         }
         .version{
-          .ft(12px, 17px);
-          margin-top: 4px;
+          .ft(.6rem, .85rem);
+          margin-top: .2rem;
         }
       }
     }
     .download-button-area{
-      margin: 80px auto 0;
-      .wh(90%, 48px);
+      margin: 4rem auto 0;
+      .wh(90%, 2.4rem);
       position: relative;
       .btn{
         position: absolute;
@@ -164,25 +177,25 @@
         .allcover();
         z-index: 2;
         background: #0DB2EF;
-        border-radius: 8px;
+        border-radius: .4rem;
         color: #fff;
         text-align: center;
-        .ft(16px, 48px);
+        .ft(.8rem, 2.4rem);
       }
       .shadow{
         position: absolute;
         z-index: 1;
         .wh(100%, 100%);
         left: 0;
-        bottom: -6px;
+        bottom: -.3rem;
         opacity: 0.05;
         background: #0DB2EF;
-        border-radius: 8px;
+        border-radius: .4rem;
       }
     }
     .tips{
-      margin-top: 17px;
-      .ft(14px, 20px);
+      margin-top: .85rem;
+      .ft(.7rem, 1rem);
       color: #999;
       text-align: center;
     }
