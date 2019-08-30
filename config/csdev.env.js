9bce159d1d45dc291617d2285d83b4962d07b863
PNFeather <921192114@qq.com>
2019-08-30 17:43:54 +0800

    添加csdev环境

--- config/csdev.env.js
+++ config/csdev.env.js
@@ -3,9 +3,9 @@ const devEnv = require('./dev.env')
 module.exports = {
   NODE_ENV: '"production"',
   MOCK: 'false',
-  BASE_URL: '"http://47.112.96.161:8080/api"', // 后端接口地址
+  BASE_URL: '"https://devapi.ecaiedu.com/api"', // 后端接口地址
   // BASE_URL: '"http://10.0.2.252:8080/api"',
-  XZDKF_ENTRANCE: '"http://47.112.96.161/service/#/login"', // 客服入口
+  XZDKF_ENTRANCE: '"https://dev.ecaiedu.com/service/#/login"', // 客服入口
   CJIMGURL: '"https://cjfc-dev.oss-cn-shenzhen.aliyuncs.com/"', // oss地址
   CONFIG_NAME: '"csdev"'
 }
