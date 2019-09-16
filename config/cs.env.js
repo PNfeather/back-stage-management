70ec0e7d88cbb5e45c1505a5ce560a1d9ca4273a
PNFeather <921192114@qq.com>
2019-09-16 11:47:35 +0800

    测试环境域名调整

--- config/cs.env.js
+++ config/cs.env.js
@@ -3,7 +3,7 @@ const devEnv = require('./dev.env')
 module.exports = {
   NODE_ENV: '"production"',
   MOCK: 'false',
-  BASE_URL: '"https://cjapi.ecaiedu.com/api"', // 后端接口地址
+  BASE_URL: '"https://devapi.ecaiedu.com/api"', // 后端接口地址
   // BASE_URL: '"http://10.0.2.252:8080/api"',
   XZDKF_ENTRANCE: '"https://www.ecaiedu.com/service/#/login"', // 客服入口
   CJIMGURL: '"https://cjfc-dev.oss-cn-shenzhen.aliyuncs.com/"', // oss地址
--- config/dev.env.js
+++ config/dev.env.js
@@ -6,7 +6,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   MOCK: 'true',
   // BASE_URL: '"/api"'
-  BASE_URL: '"https://cjapi.ecaiedu.com/api"',
+  BASE_URL: '"https://devapi.ecaiedu.com/api"',
   // BASE_URL: '"http://10.0.2.88:8080/api"',
   XZDKF_ENTRANCE: '"https://www.ecaiedu.com/service/#/login"', // 客服入口
   CJIMGURL: '"https://cjfc-dev.oss-cn-shenzhen.aliyuncs.com/"'
