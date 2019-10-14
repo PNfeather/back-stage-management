78db32cf867bada7b3b65b165a307d850a5c010a
PNFeather <921192114@qq.com>
2019-10-14 09:38:33 +0800

    白屏测试

--- config/ys.env.js
+++ config/ys.env.js
@@ -2,6 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   MOCK: 'false',
+  VCONSOLE: 'true',
   BASE_URL: '"https://demoapi.ecaiedu.com/api"',
   XZDKF_ENTRANCE: '"https://demo.ecaiedu.com/service/#/login"',
   CJIMGURL: '"https://cjfc-dev.oss-cn-shenzhen.aliyuncs.com/"',
